abc53d7c613a849dfa0a5b1d2f3f75ab182d0136
victorkingi <kingivictor77@gmail.com>
2021-09-13 21:36:57 +0300

    bug fixes

--- src/app/form-elements/InputSell.js
+++ src/app/form-elements/InputSell.js
@@ -1,20 +1,62 @@
 import React, { useEffect, useState } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { Form } from 'react-bootstrap';
 import bsCustomFileInput from 'bs-custom-file-input';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css";
-import Snackbar from "@material-ui/core/Snackbar";
-import {Alert} from "./InputEggs";
-import {getSectionAddr, inputSell} from "../../services/actions/salesAction";
-import { Offline, Online } from "react-detect-offline";
-import {firebase} from '../../services/api/fbConfig';
+import 'react-datepicker/dist/react-datepicker.css';
+import Snackbar from '@material-ui/core/Snackbar';
+import { Alert } from './InputEggs';
+import { getSectionAddr, inputSell } from '../../services/actions/salesAction';
+import { Offline, Online } from 'react-detect-offline';
+import { firebase } from '../../services/api/fbConfig';
+import Predictionary from 'predictionary';
+import ed from 'edit-distance';
 
 let name = firebase.auth().currentUser?.displayName;
-name =  name ? name.substring(0, name.lastIndexOf(" ")).toUpperCase() : '';
+name = name ? name.substring(0, name.lastIndexOf(' '))
+  .toUpperCase() : '';
+
+function autoCorrectBuyerName(entered) {
+  let insert,
+    remove,
+    update;
+  insert = remove = () => {
+    return 1;
+  };
+  update = (stringA, stringB) => {
+    return stringA !== stringB ? 1 : 0;
+  };
+  let predictionary = Predictionary.instance();
+  const skip = ['Thika Farmers', 'Cakes', 'Duka'];
+  const chosen = [
+    'Eton',
+    'Sang\'',
+    'Karithi',
+    'Titus',
+    'Mwangi',
+    'Lynn',
+    'Gituku',
+    'Lang\'at',
+    'Wahome',
+    'Kamau',
+    'Simiyu',
+    'Kinyanjui'
+  ];
+  predictionary.addWords(chosen);
+  if (skip.includes(entered)) return entered;
+  let suggestions = predictionary.predict(entered);
+  const final = [];
+  for (let k in suggestions) {
+    const chose = chosen[k];
+    const lev = ed.levenshtein(entered, chose, insert, remove, update);
+    if (lev.distance < 3) final.push(chose);
+  }
+  if (final.length === 1) return final[0];
+  if (final.length > 1 || final.length === 0) return final;
+}
 
 function InputSell(props) {
   const [state, setState] = useState({
@@ -30,11 +72,16 @@ function InputSell(props) {
   const [defPaid, setDefPaid] = useState(false);
 
   useEffect(() => {
-    if (name !== "VICTOR") setDisReplace(true);
-    else setDisReplace(false);
-    if (state.section === "Thika Farmers" || state.section === "Duka") setDefPaid(true);
-    else setDefPaid(false);
-
+    if (name !== 'VICTOR') {
+      setDisReplace(true);
+    } else {
+      setDisReplace(false);
+    }
+    if (state.section === 'Thika Farmers' || state.section === 'Duka') {
+      setDefPaid(true);
+    } else {
+      setDefPaid(false);
+    }
   }, [state]);
 
   const checkDate = (date) => {
@@ -45,11 +92,13 @@ function InputSell(props) {
     } else {
       return true;
     }
-  }
+  };
 
   const parameterChecks = (values) => {
-    if ((values.section === "THIKA_FARMERS" || values.section === "DUKA")
-        &&  !JSON.parse(values.status)) {
+    if (
+      (values.section === 'THIKA_FARMERS' || values.section === 'DUKA') &&
+      !JSON.parse(values.status)
+    ) {
       setError('Thika Farmers and duka are always paid');
       setOpenError(true);
       return false;
@@ -71,12 +120,14 @@ function InputSell(props) {
     }
     let proceed = checkDate(values.date);
     if (values.buyerName && proceed) {
-      values.buyerName = values.buyerName.charAt(0).toUpperCase().concat(values
-          .buyerName.substring(1));
+      values.buyerName = values.buyerName
+        .charAt(0)
+        .toUpperCase()
+        .concat(values.buyerName.substring(1));
       return true;
     }
     return false;
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -90,18 +141,18 @@ function InputSell(props) {
       return;
     }
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i][1] === "") {
+      if (arr[i][1] === '') {
         setError('All Inputs should be filled');
         setOpenError(true);
         return;
       }
-      if (arr[i][0] === "section") {
-        if (arr[i][1] === "Choose Section") {
+      if (arr[i][0] === 'section') {
+        if (arr[i][1] === 'Choose Section') {
           setError('Section should be selected');
           setOpenError(true);
           return;
         }
-        if (arr[i][1] !== "Other Buyer") {
+        if (arr[i][1] !== 'Other Buyer') {
           if (state.section !== state.buyerName) {
             setError('Section and buyer name should be the same');
             setOpenError(true);
@@ -115,17 +166,17 @@ function InputSell(props) {
           }
         }
       }
-      if (arr[i][0] === "trayNo" || arr[i][0] === "trayPrice") {
+      if (arr[i][0] === 'trayNo' || arr[i][0] === 'trayPrice') {
         if (!trayRegex.test(arr[i][1])) {
           setError('Tray price and amount cannot be negative');
           setOpenError(true);
           return;
         }
-      if (noZeroRegex.test(arr[i][1])) {
-        setError('Tray price and amount cannot be zero');
-        setOpenError(true);
-        return;
-      }
+        if (noZeroRegex.test(arr[i][1])) {
+          setError('Tray price and amount cannot be zero');
+          setOpenError(true);
+          return;
+        }
       }
     }
     let status = true;
@@ -137,26 +188,36 @@ function InputSell(props) {
       name,
       status,
       replaced: !!state.replaced
-    }
+    };
     delete values.not_paid;
     delete values.paid;
     values.category = 'sales';
     values.section = getSectionAddr(values.section);
-    values.buyerName = values.buyerName.charAt(0).toUpperCase().concat(values
-        .buyerName.substring(1));
-    if (name !== "VICTOR" && values.replaced) {
+    values.buyerName = values.buyerName
+      .charAt(0)
+      .toUpperCase()
+      .concat(values.buyerName.substring(1));
+    if (name !== 'VICTOR' && values.replaced) {
       setError('Untick replace wrong entry');
       setOpenError(true);
       return;
     }
     let date = new Date(values.date);
-    date.setHours(0,0,0,0);
+    date.setHours(0, 0, 0, 0);
     values.date = date;
     let proceed = parameterChecks(values);
     if (proceed) {
-      props.inputSell(values);
-      setOpenError(false);
-      setOpen(true);
+      const returned = autoCorrectBuyerName(values.buyerName);
+      if (typeof returned === 'string') {
+        values.buyerName = returned;
+        props.inputSell(values);
+        setOpenError(false);
+        setOpen(true);
+      } else {
+        props.inputSell(values, true);
+        setError('New buyer detected, this issue will be reported');
+        setOpenError(true);
+      }
     }
   };
 
@@ -165,7 +226,7 @@ function InputSell(props) {
       ...state,
       date: date
     });
-  }
+  };
 
   const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
@@ -205,134 +266,196 @@ function InputSell(props) {
         buyerName: e
       });
     }
-  }
+  };
 
   const componentDidMount = () => {
-    bsCustomFileInput.init()
-  }
+    bsCustomFileInput.init();
+  };
   useEffect(() => {
     componentDidMount();
   }, []);
 
-    if (redirect) {
-      return (
-          <Redirect to='/'/>
-      )
-    }
+  if (redirect) {
+    return <Redirect to='/' />;
+  }
 
-    return (
-        <div>
-          <div className="page-header">
-            <h3 className="page-title">Input Sales</h3>
-            <nav aria-label="breadcrumb">
-              <ol className="breadcrumb">
-                <li className="breadcrumb-item"><a style={{textDecoration: 'none'}} href="/" onClick={event => {
+  return (
+    <div>
+      <div className='page-header'>
+        <h3 className='page-title'>Input Sales</h3>
+        <nav aria-label='breadcrumb'>
+          <ol className='breadcrumb'>
+            <li className='breadcrumb-item'>
+              <a
+                style={{ textDecoration: 'none' }}
+                href='/'
+                onClick={(event) => {
                   event.preventDefault();
                   setRedirect(true);
-                }}>Home</a></li>
-                <li className="breadcrumb-item active" aria-current="page">Input Sales</li>
-              </ol>
-            </nav>
-          </div>
-          <div className="col-xl grid-margin stretch-card">
-            <div className="card">
-              <div className="card-body">
-                <h4 className="card-title">Input Sale</h4>
-                <p className="card-description"> Enter sales made </p>
-                <form className="forms-sample">
-                  <label htmlFor="date">Date</label>
-                  <Form.Group>
-                    <DatePicker
-                        selected={state.date}
-                        onChange={handleDate}
-                        className='form-control'
-                        id='date'
-                    />
-                  </Form.Group>
-                  <Form.Group>
-                    <label htmlFor="section">Section</label>
-                    <DropdownButton
-                        alignRight
-                        title={state.section || 'Choose Section'}
-                        id="dropdown-menu-align-right"
-                        onSelect={handleSelect}
-                    >
-                      <Dropdown.Item eventKey="Thika Farmers">Thika Farmers</Dropdown.Item>
-                      <Dropdown.Item eventKey="Cakes">Cakes</Dropdown.Item>
-                      <Dropdown.Item eventKey="Duka">Duka</Dropdown.Item>
-                      <Dropdown.Divider />
-                      <Dropdown.Item eventKey="Other Buyer">Other Buyer</Dropdown.Item>
-                    </DropdownButton>
-                  </Form.Group>
-                  <Form.Group>
-                    <label htmlFor="buyerName">Buyer Name</label>
-                    <Form.Control type="text"
-                                  onChange={handleSelect}
-                                  className="form-control" id="buyerName" placeholder="Name of Buyer" />
-                  </Form.Group>
-                  <Form.Group>
-                    <label htmlFor="trayNo">Number of Trays</label>
-                    <Form.Control type="number" onChange={handleSelect} className="form-control" id="trayNo" placeholder="Number of Trays" />
-                  </Form.Group>
-                  <Form.Group>
-                    <label htmlFor="trayPrice">Price per Tray</label>
-                    <Form.Control type="number" onChange={handleSelect} className="form-control" id="trayPrice" placeholder="Price per Tray" />
-                  </Form.Group>
-                  <div className="col-md-6">
-                    <Form.Group>
-                      <div className="form-check">
-                        <label htmlFor="1" className="form-check-label">
-                          <input disabled={defPaid} type="radio" onChange={handleSelect} className="form-check-input" name="status" id="paid" defaultChecked defaultValue={0}/>
-                          <i className="input-helper"/>
-                          Paid
-                        </label>
-                      </div>
-                      <div className="form-check">
-                        <label htmlFor="0" className="form-check-label">
-                          <input disabled={defPaid} type="radio" onChange={handleSelect} className="form-check-input" name="status" id="not_paid" defaultValue={0} />
-                          <i className="input-helper"/>
-                          Not Paid
-                        </label>
-                      </div>
-                      <div className="form-check">
-                        <label htmlFor="replaced" className="form-check-label text-muted">
-                          <input disabled={disReplace} type="checkbox" onChange={handleSelect} className="form-check-input" id="replaced" name="replaced" defaultValue={0} />
-                          <i className="input-helper"/>
-                          Replace an entry
-                        </label>
-                      </div>
-                    </Form.Group>
+                }}
+              >
+                Home
+              </a>
+            </li>
+            <li className='breadcrumb-item active' aria-current='page'>
+              Input Sales
+            </li>
+          </ol>
+        </nav>
+      </div>
+      <div className='col-xl grid-margin stretch-card'>
+        <div className='card'>
+          <div className='card-body'>
+            <h4 className='card-title'>Input Sale</h4>
+            <p className='card-description'> Enter sales made </p>
+            <form className='forms-sample'>
+              <label htmlFor='date'>Date</label>
+              <Form.Group>
+                <DatePicker
+                  selected={state.date}
+                  onChange={handleDate}
+                  className='form-control'
+                  id='date'
+                />
+              </Form.Group>
+              <Form.Group>
+                <label htmlFor='section'>Section</label>
+                <DropdownButton
+                  alignRight
+                  title={state.section || 'Choose Section'}
+                  id='dropdown-menu-align-right'
+                  onSelect={handleSelect}
+                >
+                  <Dropdown.Item eventKey='Thika Farmers'>
+                    Thika Farmers
+                  </Dropdown.Item>
+                  <Dropdown.Item eventKey='Cakes'>Cakes</Dropdown.Item>
+                  <Dropdown.Item eventKey='Duka'>Duka</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item eventKey='Other Buyer'>
+                    Other Buyer
+                  </Dropdown.Item>
+                </DropdownButton>
+              </Form.Group>
+              <Form.Group>
+                <label htmlFor='buyerName'>Buyer Name</label>
+                <Form.Control
+                  type='text'
+                  onChange={handleSelect}
+                  className='form-control'
+                  id='buyerName'
+                  placeholder='Name of Buyer'
+                />
+              </Form.Group>
+              <Form.Group>
+                <label htmlFor='trayNo'>Number of Trays</label>
+                <Form.Control
+                  type='number'
+                  onChange={handleSelect}
+                  className='form-control'
+                  id='trayNo'
+                  placeholder='Number of Trays'
+                />
+              </Form.Group>
+              <Form.Group>
+                <label htmlFor='trayPrice'>Price per Tray</label>
+                <Form.Control
+                  type='number'
+                  onChange={handleSelect}
+                  className='form-control'
+                  id='trayPrice'
+                  placeholder='Price per Tray'
+                />
+              </Form.Group>
+              <div className='col-md-6'>
+                <Form.Group>
+                  <div className='form-check'>
+                    <label htmlFor='1' className='form-check-label'>
+                      <input
+                        disabled={defPaid}
+                        type='radio'
+                        onChange={handleSelect}
+                        className='form-check-input'
+                        name='status'
+                        id='paid'
+                        defaultChecked
+                        defaultValue={0}
+                      />
+                      <i className='input-helper' />
+                      Paid
+                    </label>
                   </div>
-                  <button type="submit" className="btn btn-primary mr-2" onClick={handleSubmit}>Submit</button>
-                </form>
+                  <div className='form-check'>
+                    <label htmlFor='0' className='form-check-label'>
+                      <input
+                        disabled={defPaid}
+                        type='radio'
+                        onChange={handleSelect}
+                        className='form-check-input'
+                        name='status'
+                        id='not_paid'
+                        defaultValue={0}
+                      />
+                      <i className='input-helper' />
+                      Not Paid
+                    </label>
+                  </div>
+                  <div className='form-check'>
+                    <label
+                      htmlFor='replaced'
+                      className='form-check-label text-muted'
+                    >
+                      <input
+                        disabled={disReplace}
+                        type='checkbox'
+                        onChange={handleSelect}
+                        className='form-check-input'
+                        id='replaced'
+                        name='replaced'
+                        defaultValue={0}
+                      />
+                      <i className='input-helper' />
+                      Replace an entry
+                    </label>
+                  </div>
+                </Form.Group>
               </div>
-            </div>
+              <button
+                type='submit'
+                className='btn btn-primary mr-2'
+                onClick={handleSubmit}
+              >
+                Submit
+              </button>
+            </form>
           </div>
-          <Online>
-            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-              <Alert onClose={handleClose} severity="success">
-                Data Submitted
-              </Alert>
-            </Snackbar>
-          </Online>
-          <Offline>
-            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-              <Alert onClose={handleClose} severity="warning">
-                Data will be submitted automatically when back online
-              </Alert>
-            </Snackbar>
-          </Offline>
-          <Snackbar open={openError} autoHideDuration={6000} onClose={handleClose}>
-            <Alert severity="error">{error}!</Alert>
-          </Snackbar>
         </div>
-    )
+      </div>
+      <Online>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity='success'>
+            Data Submitted
+          </Alert>
+        </Snackbar>
+      </Online>
+      <Offline>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity='warning'>
+            Data will be submitted automatically when back online
+          </Alert>
+        </Snackbar>
+      </Offline>
+      <Snackbar open={openError} autoHideDuration={6000} onClose={handleClose}>
+        <Alert severity='error'>{error}!</Alert>
+      </Snackbar>
+    </div>
+  );
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     inputSell: (sale) => dispatch(inputSell(sale))
-  }
-}
+  };
+};
 
-export default connect(null, mapDispatchToProps)(InputSell)
+export default connect(null, mapDispatchToProps)(InputSell);
--- src/services/actions/salesAction.js
+++ src/services/actions/salesAction.js
@@ -1,48 +1,76 @@
 function makeid(l) {
-    let text = "";
-    const char_list = '!#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_abcdefghijklmnopqrstuvwxyz{|}~';
-    for (let i = 0; i < l; i++) {
-        text += char_list.charAt(Math.floor(Math.random() * char_list.length));
-    }
-    return text;
+  let text = '';
+  const char_list = '!#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_abcdefghijklmnopqrstuvwxyz{|}~';
+  for (let i = 0; i < l; i++) {
+    text += char_list.charAt(Math.floor(Math.random() * char_list.length));
+  }
+  return text;
 }
 
 export const getSectionAddr = (section) => {
-    let newSection = section.toUpperCase();
-    const index = newSection.indexOf(' ');
-    if (section === 'Other Buyer') return newSection.substring(0, 5).concat('_SALE');
-    else if (section === 'Other Purchase') return newSection.substring(0, 5).concat('_BUY');
-    else if (section === 'Purity') return 'OTHER_PURITY';
-    if (index < 0) return newSection;
-    return newSection.substring(0, index).concat('_', newSection.substring(index+1))
-}
+  let newSection = section.toUpperCase();
+  const index = newSection.indexOf(' ');
+  if (section === 'Other Buyer') {
+    return newSection.substring(0, 5)
+      .concat('_SALE');
+  } else if (section === 'Other Purchase') {
+    return newSection.substring(0, 5)
+      .concat('_BUY');
+  } else if (section === 'Purity') return 'OTHER_PURITY';
+  if (index < 0) return newSection;
+  return newSection.substring(0, index)
+    .concat('_', newSection.substring(index + 1));
+};
 
 /**
  * if other sales exists in pending_transactions, query and get total
  * trays sold, - from current trays and use the result as value
  * @returns {function(*, *, {getFirebase: *, getFirestore: *}): void}
  * @param values
+ * @param isUnknownBuyer
  */
-export const inputSell = (values) => {
-    return (dispatch, getState, {getFirebase, getFirestore}) => {
-        const firestore = getFirestore();
-        console.log(values)
-        if (JSON.parse(values.status)) {
-            firestore.collection("pending_transactions").add({
-                values: values,
-                submittedOn: new Date()
-            });
-            dispatch({type: 'INPUT_SALES', values});
+export const inputSell = (values, isUnknownBuyer) => {
+  return (dispatch, getState, {
+    getFirebase,
+    getFirestore
+  }) => {
+    const firestore = getFirestore();
+    if (!isUnknownBuyer) {
+      if (JSON.parse(values.status)) {
+        firestore.collection('pending_transactions')
+          .add({
+            values: values,
+            submittedOn: new Date()
+          });
+        dispatch({
+          type: 'INPUT_SALES',
+          values
+        });
 
-        } else if (!JSON.parse(values.status)) {
-            firestore.collection("late_payment").add({
-                key: makeid(28),
-                values,
-                submittedOn: new Date()
-            });
-            dispatch({type: 'INPUT_SALES', values});
-        }
+      } else if (!JSON.parse(values.status)) {
+        firestore.collection('late_payment')
+          .add({
+            key: makeid(28),
+            values,
+            submittedOn: new Date()
+          });
+        dispatch({
+          type: 'INPUT_SALES',
+          values
+        });
+      }
+    } else {
+      firestore.collection('pending_transactions')
+        .add({
+          values: values,
+          weirdName: new Date()
+        });
+      dispatch({
+        type: 'INPUT_SALES',
+        values
+      });
     }
-}
+  };
+};
 
-export {makeid};
+export { makeid };
